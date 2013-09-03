52157cd36446e91007b90155c8bb2916d9c9c0eb
Matt Butterfield <mbutterfield@onshift.com>
2013-09-03 19:48:52 -0400

    allow new users to sign up

--- rideup/views.py
+++ rideup/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
 from rideup.models import Ride, CreateRideForm
 
@@ -29,6 +31,7 @@ def create(request):
 def showrides(request):
     return render(request, 'rideup/showrides.html')
 
+
 def get_map_rides(request):
     if request.is_ajax():
         northeastLat = request.GET['northeastLat']
@@ -51,3 +54,19 @@ def get_map_rides(request):
                             mimetype='application/json')
     else:
         return HttpResponse()
+
+
+def register(request):
+    form = UserCreationForm(request.POST.copy())
+    if form.is_valid():
+        form.save()
+        new_user = authenticate(username=request.POST['username'],
+                                password=request.POST['password1'])
+        login(request, new_user)
+        return HttpResponseRedirect('/register_success/')
+    else:
+        return HttpResponseRedirect('/')
+
+@login_required
+def register_success(request):
+    return render(request, 'rideup/register_success.html')
